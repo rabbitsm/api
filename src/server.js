d4b51e184ca09bac6d7633479ec87e3cd8cd37d2
maddhruv <dhruvjainpenny@gmail.com>
2018-10-26 07:55:41 +0530

    initial setup

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import restify from 'restify'
+
+const server = restify.createServer()
+
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(`> Server listening at ${process.env.SERVER_PORT}`)
+})
