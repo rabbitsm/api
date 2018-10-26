bee0573099c46663dcc48be4a6b131933ce5db96
maddhruv <dhruvjainpenny@gmail.com>
2018-10-26 08:20:37 +0530

    db connection

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const DB_URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_URL}`
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true
+})
+
+const connection = mongoose.connection
+
+connection.on('open', () => {
+  console.log('> MongoDB Connected')
+})
+
+connection.on('error', (err) => {
+  console.error(err)
+})
